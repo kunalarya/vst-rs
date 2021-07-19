cef35ba3624fa5f5f8cbca2af2d19424b22b0d76
Kunal Arya <me@example.com>
2021-07-18 20:40:58 -0700

    Patches & formatting.

--- src/plugin.rs
+++ src/plugin.rs
@@ -826,9 +826,7 @@ impl HostCallback {
         ptr: *mut c_void,
         opt: f32,
     ) -> isize {
-        let callback = self
-            .callback
-            .unwrap_or_else(|| panic!("Host not yet initialized."));
+        let callback = self.callback.unwrap_or_else(|| panic!("Host not yet initialized."));
         callback(effect, opcode.into(), index, value, ptr, opt)
     }
 
@@ -850,14 +848,7 @@ impl HostCallback {
 
     /// Get the VST API version supported by the host e.g. `2400 = VST 2.4`.
     pub fn vst_version(&self) -> i32 {
-        self.callback(
-            self.effect,
-            host::OpCode::Version,
-            0,
-            0,
-            ptr::null_mut(),
-            0.0,
-        ) as i32
+        self.callback(self.effect, host::OpCode::Version, 0, 0, ptr::null_mut(), 0.0) as i32
     }
 
     /// Get the callback for calling host-specific extensions
@@ -876,23 +867,9 @@ impl HostCallback {
         self.read_string_param(opcode, 0, 0, 0.0, max)
     }
 
-    fn read_string_param(
-        &self,
-        opcode: host::OpCode,
-        index: i32,
-        value: isize,
-        opt: f32,
-        max: usize,
-    ) -> String {
+    fn read_string_param(&self, opcode: host::OpCode, index: i32, value: isize, opt: f32, max: usize) -> String {
         let mut buf = vec![0; max];
-        self.callback(
-            self.effect,
-            opcode,
-            index,
-            value,
-            buf.as_mut_ptr() as *mut c_void,
-            opt,
-        );
+        self.callback(self.effect, opcode, index, value, buf.as_mut_ptr() as *mut c_void, opt);
         String::from_utf8_lossy(&buf)
             .chars()
             .take_while(|c| *c != '\0')
@@ -901,31 +878,30 @@ impl HostCallback {
 }
 
 impl Host for HostCallback {
+    /// Signal the host that the value for the parameter has changed.
+    ///
+    /// Make sure to also call `begin_edit` and `end_edit` when a parameter
+    /// has been touched. This is important for the host to determine
+    /// if a user interaction is happening and the automation should be recorded.
     fn automate(&mut self, index: i32, value: f32) {
-        // log::info!("Automate: index={} value={}", index, value);
-        // log::info!("Automate: is_effect_valid()? {:?}", self.is_effect_valid());
         if self.is_effect_valid() {
             // TODO: Investigate removing this check, should be up to host
-            self.callback(
-                self.effect,
-                host::OpCode::Automate,
-                index,
-                0,
-                ptr::null_mut(),
-                value,
-            );
+            self.callback(self.effect, host::OpCode::Automate, index, 0, ptr::null_mut(), value);
         }
+        }
+
+    /// Signal the host the start of a parameter change a gesture (mouse down on knob dragging).
+    fn begin_edit(&self, index: i32) {
+        self.callback(self.effect, host::OpCode::BeginEdit, index, 0, ptr::null_mut(), 0.0);
+    }
+
+    /// Signal the host the end of a parameter change gesture (mouse up after knob dragging).
+    fn end_edit(&self, index: i32) {
+        self.callback(self.effect, host::OpCode::EndEdit, index, 0, ptr::null_mut(), 0.0);
     }
 
     fn get_plugin_id(&self) -> i32 {
-        self.callback(
-            self.effect,
-            host::OpCode::CurrentId,
-            0,
-            0,
-            ptr::null_mut(),
-            0.0,
-        ) as i32
+        self.callback(self.effect, host::OpCode::CurrentId, 0, 0, ptr::null_mut(), 0.0) as i32
     }
 
     fn idle(&self) {
@@ -934,14 +910,7 @@ impl Host for HostCallback {
 
     fn get_info(&self) -> (isize, String, String) {
         use api::consts::*;
-        let version = self.callback(
-            self.effect,
-            host::OpCode::CurrentId,
-            0,
-            0,
-            ptr::null_mut(),
-            0.0,
-        ) as isize;
+        let version = self.callback(self.effect, host::OpCode::CurrentId, 0, 0, ptr::null_mut(), 0.0) as isize;
         let vendor_name = self.read_string(host::OpCode::GetVendorString, MAX_VENDOR_STR_LEN);
         let product_name = self.read_string(host::OpCode::GetProductString, MAX_PRODUCT_STR_LEN);
         (version, vendor_name, product_name)
@@ -989,14 +958,12 @@ impl Host for HostCallback {
 
     /// Get block size.
     fn get_block_size(&self) -> isize {
-        self.callback(
-            self.effect,
-            host::OpCode::GetBlockSize,
-            0,
-            0,
-            ptr::null_mut(),
-            0.0,
-        )
+        self.callback(self.effect, host::OpCode::GetBlockSize, 0, 0, ptr::null_mut(), 0.0)
+    }
+
+    /// Refresh UI after the plugin's parameters changed.
+    fn update_display(&self) {
+        self.callback(self.effect, host::OpCode::UpdateDisplay, 0, 0, ptr::null_mut(), 0.0);
     }
 }
 
@@ -1033,7 +1000,7 @@ mod tests {
 
                 fn new(host: HostCallback) -> TestPlugin {
                     TestPlugin {
-                        host: host
+                        host
                     }
                 }
 
@@ -1042,7 +1009,9 @@ mod tests {
                     assert_eq!(2400, self.host.vst_version());
                     assert_eq!(9876, self.host.get_plugin_id());
                     // Callback will assert these.
+                    self.host.begin_edit(123);
                     self.host.automate(123, 12.3);
+                    self.host.end_edit(123);
                     self.host.idle();
                 }
             }
@@ -1058,11 +1027,19 @@ mod tests {
                                  -> isize {
                     let opcode = OpCode::from(opcode);
                     match opcode {
+                        OpCode::BeginEdit => {
+                            assert_eq!(index, 123);
+                            0
+                        },
                         OpCode::Automate => {
                             assert_eq!(index, 123);
                             assert_eq!(opt, 12.3);
                             0
-                        }
+                        },
+                        OpCode::EndEdit => {
+                            assert_eq!(index, 123);
+                            0
+                        },
                         OpCode::Version => 2400,
                         OpCode::CurrentId => 9876,
                         OpCode::Idle => 0,
@@ -1102,13 +1079,6 @@ mod tests {
     #[test]
     fn host_callbacks() {
         let aeffect = instance();
-        (unsafe { (*aeffect).dispatcher })(
-            aeffect,
-            plugin::OpCode::Initialize.into(),
-            0,
-            0,
-            ptr::null_mut(),
-            0.0,
-        );
+        (unsafe { (*aeffect).dispatcher })(aeffect, plugin::OpCode::Initialize.into(), 0, 0, ptr::null_mut(), 0.0);
     }
 }
